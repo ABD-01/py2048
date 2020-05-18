e029d8a4ce319d62711ee5426ac4f0bcc36c72b1
ABD-01 <myac931@gmail.com>
2020-05-18 04:26:13 +0000

    2048 game completed

---
+++ 2048.py
@@ -0,0 +1,120 @@
+from sys import exit
+from os import system
+import argparse
+#from msvcrt import getch
+
+# My own defined module consisting of functions such as transpose,inverse, print,etc
+from essentials import *
+
+
+# Checking for command line arguments with default board dimension 5x5 and default win criteria 2048
+parser = argparse.ArgumentParser(description='Enter Board_Size and Win_criteria')
+parser.add_argument('-n', '--size', type=int, default=5, help='Size of Board')
+parser.add_argument('-w', '--win', type=int, default=2048, help='Win Criteria')
+args = parser.parse_args()
+board_size = int(args.size)
+win_cond = Powerof2(args.win)
+##print("Win criteria is",win_cond)
+
+# Initializing the board with all zeros and one 2 at random position
+game_board = [[0]*board_size for _ in range(board_size)]
+game_board = insert_two(game_board)
+game_board = insert_two(game_board)
+system('clear')
+printboard(game_board)
+
+
+# Check for input Key
+def getKey():
+    ch = getch().lower()
+    while True:
+
+        # checking if entered key is correct
+        if ch in ['a', 's', 'd', 'w']:
+            if isValid(ch, game_board) == False:
+                print("Invalid move = ", ch)
+                ch = getch().lower()
+                continue
+            else:
+                ##print("You pressed", ch)
+                return ch
+
+        # If user wants to quit b/w gthe game
+        elif ch == 'q':
+            print("Do you want to Exit the game ?(y/n)")
+            if input() in ['y', 'yes']:
+                exit(1)
+            else:
+                printboard(game_board)
+                ch = getch().lower()
+
+        # In anyother case Re-propmt
+        else:
+            print("Invalid Key = ", ch)
+            ch = getch().lower()
+            continue
+
+
+# Most Important part of the program
+def game_move(ch, gboard):
+
+    # Orienting the board so as all the actions are simply Left to Right
+    if ch == 'd':
+        board = gboard
+    elif ch == 'a':
+        board = invert(gboard)
+    elif ch == 's':
+        board = transpose(gboard)
+    elif ch == 'w':
+        board = transpose(gboard)
+        board = invert(board)
+
+    # Moving all Non-Zero elements to Rigth
+    for row in board:
+        row.sort(key=bool)
+
+        #  merging two consecutive equal values
+        i = len(row)-1
+        while i >= 0:
+            if row[i] == row[i-1]:
+                row[i] = 2*row[i]
+                row[i-1] = 0
+                i -= 2
+            else:
+                i -= 1
+
+    # Again moving all Non-zero elements to right
+    for row in board:
+        row.sort(key=bool)
+
+    # Re-orienting the board back to the form it was
+    if ch == 'w':
+        board = invert(board)
+        board = transpose(board)
+    elif ch == 'a':
+        board = invert(board)
+    elif ch == 's':
+        board = transpose(board)
+
+    return board
+
+
+# Similar to main function
+while True:
+
+    ch = getKey()
+    system('clear')
+    game_board = game_move(ch, game_board)
+    game_board = insert_two(game_board)
+    printboard(game_board)
+
+    # Checking the game progress
+    if didLose(game_board) == True:
+        print('Game Over')
+        exit(0)
+
+    # checking for winning Criteria
+    if any(win_cond in row for row in game_board) == True:
+        print('You Win')
+        exit(0)
+
---
+++ essentials.py
@@ -0,0 +1,107 @@
+''' Defining getch() function
+    as msvcrt module doesn't work in Ubuntu'''
+import sys, termios, tty, os, time
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+'''DISCLAIMER: THE ABOVE CODE FOR getch() IS TAKEN FROM INTERNET'''
+
+
+# Highest power of two less than or equal to a numebr
+def Powerof2(n):
+    power = 1
+    for i in range(n,0,-1):
+        if not i&(i-1):
+            power = i
+            break
+    return power
+
+
+# Inserting 2 in random in board
+from random import randrange
+def insert_two(board):
+    i = randrange(len(board))
+    j = randrange(len(board))
+    while board[i][j] != 0:
+        i = randrange(len(board))
+        j = randrange(len(board))
+    board[i][j] = 2
+    return board
+
+
+# Function to Transpose a 2D List
+def transpose(board):
+    return [list(row) for row in zip(*board)]
+
+# Function to Flip the 2D List
+def invert(board):
+	return [row[::-1] for row in board]
+
+
+# Validating the Move
+def isValid(move, gboard):
+    # Orienting as the convinience
+    if move in ['w', 's']: board = transpose(gboard)
+    else: board = gboard
+
+    # Checking for any consecutive equal values
+    for row in board:
+        if any(row[i] == row[i+1] and row[i] != 0 for i in range(len(row)-1)):
+            ##print("Found consecutive equal value")
+            return True
+
+    if move in ['a', 'w']:
+        board = invert(board)
+
+    for row in board:
+        nrow = sorted(row, key=bool)
+        # Else checking is the previous state and state after the move is same or different
+        if row != nrow:
+            ##print("Check here", row)
+            return True
+
+     # If none of the above case happens then move in Invalid
+    ##print("Cannot move in given Direction", move)
+    return False
+
+
+# Pretty Printing the Game Board
+def printboard(gboard):
+    board = []
+    for row in gboard:
+        board.append([i if i != 0 else "__" for i in row])
+    print('--------'*len(board))
+    for row in board:
+        print('|', '\t| '.join([str(cell) for cell in row]) ,end='\t|\n')
+        print('--------'*len(board))
+
+
+# Checking if users lost or not
+def didLose(board):
+    # Check for any zero present
+    if any(0 in row for row in board) == True:
+        ##print("There is still zero present,You didn't lose")
+        return False
+
+    # Checking for any eual consecutive Values in rows
+    for row in board:
+        if any(row[i] == row[i+1] for i in range(len(row)-1)):
+            ##print("consecutive equal values exists, Didn'i lose")
+            return False
+
+    # Checking for any equal consecutive Values in columns
+    board = transpose(board)
+    for row in board:
+        if any(row[i] == row[i+1] for i in range(len(row)-1)):
+            ##print("consecutive equal values exists, Didn'i lose")
+            return False
+
+    ##print("No move is Possible")
+    return True
